575f3fc89850a02916451e1022d3e5ede548ed8d
shahabhishekofficial-create <shahabhishekofficial@gmail.com>
2026-09-08 01:33:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
-  url: "https://xwjfanvkdhpiuzmkczvo.supabase.co",
+  url: "https://pbhkuofylhqcqmspubmb.supabase.co",
   key: "sb_publishable_od_hrR-3-scS1xZTi2bMmg_330DGAlV"
 };
