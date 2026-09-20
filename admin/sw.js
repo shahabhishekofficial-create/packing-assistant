786db834f51b532379badd8778a2ec331985abb2
shahabhishekofficial-create <shahabhishekofficial@gmail.com>
2026-09-20 18:50:45 +0530

    Refresh Admin PWA service worker cache

--- admin/sw.js
+++ admin/sw.js
@@ -1,4 +1,4 @@
-const CACHE="packing-assistant-admin-v1";
+const CACHE="packing-assistant-admin-v2";
 const CORE=["./","./index.html","./manifest.webmanifest","../styles.css","../app.js","../config.js","../icon-192.png","../icon-512.png","../icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
