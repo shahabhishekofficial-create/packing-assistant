b5758dab714022bf26f461d8d8584c138667b722
shahabhishekofficial-create <shahabhishekofficial@gmail.com>
2026-09-25 12:23:26 +0530

    Synchronize app update build version

--- update.js
+++ update.js
@@ -1,5 +1,5 @@
 (()=>{"use strict";
-const BUILD_ID="20260925-14",VERSION_URL=window.PA_VERSION_URL||"version.json";
+const BUILD_ID="20260925-15",VERSION_URL=window.PA_VERSION_URL||"version.json";
 let checking=false,pendingBuild="";
 function removeNotice(){document.getElementById("paUpdateNotice")?.remove()}
 function showNotice(remote){
