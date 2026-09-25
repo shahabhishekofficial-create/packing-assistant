f9cdb7eaa572d1d242dd3b78b960a02343a2005d
shahabhishekofficial-create <shahabhishekofficial@gmail.com>
2026-09-25 11:05:53 +0530

    Release inventory section functionality fix build 20260925-11

--- update.js
+++ update.js
@@ -1,5 +1,5 @@
 (()=>{"use strict";
-const BUILD_ID="20260925-10",VERSION_URL=window.PA_VERSION_URL||"version.json";
+const BUILD_ID="20260925-11",VERSION_URL=window.PA_VERSION_URL||"version.json";
 let checking=false,pendingBuild="";
 function removeNotice(){document.getElementById("paUpdateNotice")?.remove()}
 function showNotice(remote){
